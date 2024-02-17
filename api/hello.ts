e2aebb8f42ec0e38f0a0b6d1fd0bd389594f8b96
DaniBenavi <danibenavi1907@gmail.com>
2024-02-16 22:57:58 -1200

    fix hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,15 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { readJSON } from '../utils'
 
-const moviesJs = readJSON('../movies.json')
-// export default function handler(req: VercelRequest, res: VercelResponse) {
-//   const { name = 'World' } = req.query
-//   return res.json({
-//     message: `Hello ${name}!`
-//   })
-// }
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { movies = moviesJs } = req.query
-
-  return res.json(movies)
+  const { name = 'World' } = req.query
+  return res.json({
+    message: `Hello ${name}!`
+  })
 }
