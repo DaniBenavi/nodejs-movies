6794d61ae84004179954c7820fd52934afbe5361
DaniBenavi <danibenavi1907@gmail.com>
2024-02-16 22:53:58 -1200

    fis changes in hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,15 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { readJSON } from '../utils'
 
+const moviesJs = readJSON('../movies.json')
+// export default function handler(req: VercelRequest, res: VercelResponse) {
+//   const { name = 'World' } = req.query
+//   return res.json({
+//     message: `Hello ${name}!`
+//   })
+// }
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`
-  })
+  const { movies = moviesJs } = req.query
+
+  return res.json(movies)
 }
